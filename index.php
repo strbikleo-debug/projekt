daf1b027bb6221becf1e965b787e06a8f61e8638
strbikleo-debug <strbikleo@gmail.com>
2026-04-20 08:21:53 +0200

    update add a delete

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>projekt php</title>
 </head>
 <body>
     <form action="" method="POST">
@@ -11,13 +11,11 @@
         <input type="text" name="text">
         <button type="submit" name="add">Add</button>
     </form>
-    <?php
-    if(isset($_POST['add'])) {
-        $text = $_POST['text'];
-        $title = $_POST['title'];
-        $sql = "INSERT INTO tasks (title, description) VALUES ('$title', '$text')";
-    }
-
+    <form action="" method="POST">
+        <button type="submit" name="delete">Delete</button>
+    </form>
+    <?php 
     ?>
+    
 </body>
 </html>
